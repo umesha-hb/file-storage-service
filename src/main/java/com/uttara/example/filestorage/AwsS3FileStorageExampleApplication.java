d1046daba3068566e76de2601a5fdb8c55a94e45
umesha-hb <umesha.kit@gmail.com>
2023-05-18 11:59:02 +0530

    first commit

---
+++ src/main/java/com/uttara/example/filestorage/AwsS3FileStorageExampleApplication.java
@@ -0,0 +1,13 @@
+package com.uttara.example.filestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsS3FileStorageExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsS3FileStorageExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uttara/example/filestorage/AwsS3FileStorageExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uttara.example.filestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsS3FileStorageExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
